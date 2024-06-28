714b0bad84f5e4acbcaf30fb3ebb9a666f6e7cf3
daisuke-w <w.daisuke.001@gmail.com>
2024-06-28 16:43:26 +0900

    バリデーション、アソシエーション定義

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,14 @@
 class Note < ApplicationRecord
+  # エラーメッセージ定義
+  error_message = "can't be blank"
+
+  with_options presence: true do
+    validates :category_id,       numericality: { other_than: 1, message: error_message }
+    validates :title
+    validates :content
+    validates :is_public
+  end
+
+  belongs_to :user
+  has_many   :prompts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+
+  has_many :notes
+  has_many :prompts
 end
