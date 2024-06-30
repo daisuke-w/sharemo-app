2432b611da06cbf4dd140b38e16d13bb73fcb78b
daisuke-w <w.daisuke.001@gmail.com>
2024-06-30 17:03:11 +0900

    rubocop対応

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -4,7 +4,7 @@ class Prompt < ApplicationRecord
   error_message = "can't be blank"
 
   with_options presence: true do
-    validates :category_id,       numericality: { other_than: 1, message: error_message }
+    validates :category_id, numericality: { other_than: 1, message: error_message }
     validates :title
     validates :content
     validates :is_public
