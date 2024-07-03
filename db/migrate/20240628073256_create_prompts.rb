ed57e8beec9d714742c7f45b2820cee1e2860be7
daisuke-w <w.daisuke.001@gmail.com>
2024-07-03 17:12:56 +0900

    カラム名修正

--- db/migrate/20240628073256_create_prompts.rb
+++ db/migrate/20240628073256_create_prompts.rb
@@ -4,7 +4,7 @@ class CreatePrompts < ActiveRecord::Migration[7.0]
       t.references :user,       null: false, foreign_key: true
       t.integer :category_id,   null: false
       t.string :title,          null: false
-      t.text :prompt,           null: false
+      t.text :content,          null: false
       t.boolean :is_public,     null: false, default: false
       t.timestamps
     end
