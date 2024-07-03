b786ec883ad825afd9f5d90079a3365197cc5afe
daisuke-w <w.daisuke.001@gmail.com>
2024-07-03 19:19:47 +0900

    プロンプトコントローラー実装

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -1,2 +1,55 @@
 class PromptsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_prompt, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: :edit
+
+  def new
+    @prompt = Prompt.new
+  end
+
+  def create
+    @prompt = Prompt.new(prompt_params)
+    if @prompt.save
+      redirect_to notes_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @prompt.update(prompt_params)
+      redirect_to note_path(@prompt)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    # ログインユーザーとプロンプト保有者が別人の場合はなにもしない
+    return if current_user != @prompt.user
+
+    @prompt.destroy
+    redirect_to notes_path
+  end
+
+  private
+
+  def prompt_params
+    params.require(:prompt).permit(:title, :content, :category_id, :is_public).merge(user_id: current_user.id)
+  end
+
+  def find_prompt
+    @prompt = Prompt.find(params[:id])
+  end
+
+  def move_to_index
+    # ログインユーザーとプロンプト保有者が別人の場合は一覧ページに遷移
+    redirect_to notes_path if current_user != @prompt.user
+  end
 end
