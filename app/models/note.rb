47311328f327089a5ac6322ef727f85294890af3
daisuke-w <w.daisuke.001@gmail.com>
2024-07-05 16:02:17 +0900

    アソシエーション修正
    ・複数形に変更

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,6 +4,6 @@ class Note < ApplicationRecord
   belongs_to :user
   belongs_to :prompt, optional: true
   belongs_to :category
-  has_many   :note_tag
-  has_many   :tags, through: :note_tag
+  has_many   :note_tags
+  has_many   :tags, through: :note_tags
 end
--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -2,8 +2,8 @@ class Prompt < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
-  has_many   :note
+  has_many   :notes
   belongs_to :category
-  has_many   :prompt_tag
-  has_many   :tags, through: :prompt_tag
+  has_many   :prompt_tags
+  has_many   :tags, through: :prompt_tags
 end
